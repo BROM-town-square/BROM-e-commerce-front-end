cdd4916d9f4d4f2697b7f7bc119ad6dba7030d3d
joy.mitchel@student.moringaschool.com <JoyMitchel>
2025-06-24 23:46:22 +0300

    home.jsx

---
+++ e-commerce/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Loadpageimg from './Loadpageimg'
+
+const Home = () => {
+  return (
+    <div className='home-container'>
+        <Loadpageimg />
+
+    </div>
+  )
+}
+
+export default Home
