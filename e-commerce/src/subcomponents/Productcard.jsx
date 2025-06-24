8be604c5dc271e9e441211ab77d27d086fc5590a
Njuguna <engbrian01@gmail.com>
2025-06-24 21:55:58 +0300

    added Productcard component to render product list with individual product display and cart support

--- e-commerce/src/subcomponents/Productcard.jsx
+++ e-commerce/src/subcomponents/Productcard.jsx
@@ -10,3 +10,38 @@ const IndividualProduct = ({image,name,price,product, addToCart }) => {
     e.stopPropagation(); 
     addToCart(product);
   };
+
+    return(
+
+    <div className='individualprod'>
+        <img src={image} alt="Product Image" onClick={handleClick} />
+        <h2>{name}</h2>
+        <p>Price: ${price}</p>
+        <button onClick={handleAddToCart}>Add to cart</button>
+    </div>
+  )
+
+}
+
+const Productcard = ({products,addToCart }) => {
+  console.log(products)
+   
+  return (
+    <div className='productdiv'>
+        {products.map((product,id) => (
+            <IndividualProduct 
+            key={id}
+            image={product.image}
+            name={product.name}
+            price={product.price}
+            product={product}
+            addToCart={addToCart}
+            
+            />
+        ))}
+
+    </div>
+  )
+}
+
+export default Productcard
