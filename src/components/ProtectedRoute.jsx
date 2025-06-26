b1edc9549c36c4fa59491d7d8bbef42b3d4b6ca4
Richard Wasonga <rowwasonga@gmail.com>
2025-06-26 13:15:26 +0300

    admin has acess to all routes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem('userToken');
-  return isAuthenticated ? children : <Navigate to="/auth-required" />;
+  const userToken = localStorage.getItem('userToken');
+  const adminToken = localStorage.getItem('adminToken');
+
+  if (!userToken && !adminToken) {
+    return <Navigate to="/auth-required" replace />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
